3fdb56ffa5ff5263c5c83fe266eb43973e6e4421
shubham sharma <shubham.swapit@gmail.com>
2024-09-10 16:29:10 +0530

    removed space

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,6 @@ use Illuminate\Support\Facades\Route;
 		|
 	*/
 Route::any('ping', [App\Http\Controllers\API\RegisterController::class, 'ping']);
-
 Route::get('/csrf-token', function () {
     return response()->json(['csrf_token' => csrf_token()]);
 });
