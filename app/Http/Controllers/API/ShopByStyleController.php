207aa11a8e4f47337d6345a7b35123015701e3cc
shubham sharma <shubham.swapit@gmail.com>
2024-09-30 15:11:12 +0530

    added

---
+++ app/Http/Controllers/API/ShopByStyleController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\ShopByStyle;
+
+class ShopByStyleController extends Controller
+{
+    public function index()
+    {
+        $output['res'] = 'success';
+        $output['msg'] = 'data retrieved successfully';
+
+        $data = ShopByStyle::orderBy('order_number', 'asc')->where('status', 'true')->get();
+        foreach ($data as $banner) {
+            $banner->image = env('AWS_URL') . 'public/' . $banner->banner;
+        }
+        $output['data'] = $data;
+        return response()->json($output)
+            ->header('Cache-Control', 'max-age=86400, public');
+    }
+}
