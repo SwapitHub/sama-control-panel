0f0f90f1c6423e226fda71623c474dd6e410d807
shubham sharma <shubham.swapit@gmail.com>
2024-09-30 18:22:17 +0530

    added vsd listing api from sama apis

--- app/Exports/ProductExport.php
+++ app/Exports/ProductExport.php
@@ -7,32 +7,41 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Schema;
 use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\FromQuery;
 
-class ProductExport implements FromCollection
+// class ProductExport implements FromCollection
+class ProductExport implements FromQuery, WithHeadings
 {
     /**
     * @return \Illuminate\Support\Collection
     */
-    public function collection()
+    // public function collection()
+    // {
+    //     // return ProductModel::all();
+    //     $data = ProductModel::all();
+
+    //     // Get the column names from the database schema
+    //     $columns = Schema::getColumnListing((new ProductModel())->getTable());
+
+    //     // Map the column names to the actual database columns and remove any unwanted columns
+    //     $columns = array_map(function ($column) {
+    //         // Customize column names if needed
+    //         return $column;
+    //     }, $columns);
+
+    //     // Add column names as the first row in the collection
+    //     $data->prepend($columns);
+
+    //     return $data;
+    // }
+
+    public function query()
     {
-        // return ProductModel::all();
-        $data = ProductModel::all();
+        return ProductModel::query();
+    }
 
-        // Get the column names from the database schema
-        // $columns = Schema::getColumnListing((new ProductModel())->getTable());
-        $columns = array_map('strtolower', Schema::getColumnListing((new ProductModel())->getTable()));
-
-        dd($columns);
-
-        // Map the column names to the actual database columns and remove any unwanted columns
-        $columns = array_map(function ($column) {
-            // Customize column names if needed
-            return $column;
-        }, $columns);
-
-        // Add column names as the first row in the collection
-        $data->prepend($columns);
-
-        return $data;
+    public function headings(): array
+    {
+        return \Schema::getColumnListing((new ProductModel())->getTable());
     }
 }
