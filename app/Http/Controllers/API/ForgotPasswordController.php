26f4b0292d81840ff60c9c294ee385effcc01233
shubham sharma <shubham.swapit@gmail.com>
2024-10-10 12:29:59 +0530

    change validation for send reset password link

--- app/Http/Controllers/API/ForgotPasswordController.php
+++ app/Http/Controllers/API/ForgotPasswordController.php
@@ -1,10 +1,12 @@
 <?php
+
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Password;
 use Illuminate\Validation\ValidationException;
+use Validator;
 
 class ForgotPasswordController extends Controller
 {
@@ -16,25 +18,55 @@ class ForgotPasswordController extends Controller
      */
     public function sendResetLinkEmail(Request $request)
     {
-        // Validate the incoming request data
-        $request->validate([
-            'email' => 'required|email',
+        // $validator = Validator::make($request->all(), [
+        //     'email' => 'required|email|unique:users',
+        // ]);
+
+        // if ($validator->fails()) {
+        //     $output['res'] = 'error';
+        //     $output['msg'] = $validator->errors()->all();
+        //     $output['data'] = [];
+        //     return response()->json($output, 400);
+        // }
+        // $status = Password::sendResetLink(
+        //     $request->only('email')
+        // );
+        // if ($status === Password::RESET_LINK_SENT) {
+        //     return response()->json(['res'=>'success','msg' => __($status)], 200);
+        // }
+        // throw ValidationException::withMessages([
+        //     'email' => [trans($status)],
+        // ]);
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email|exists:users,email', // Check that the email exists in the users table
+        ], [
+            'email.exists' => 'We can\'t find a user with that email address.', // Custom error message
         ]);
 
-        // Send the password reset link to the user's email
+        // If validation fails, return error messages
+        if ($validator->fails()) {
+            return response()->json([
+                'res' => 'error',
+                'msg' => $validator->errors()->first(), // Return the first error message
+                'data' => []
+            ], 400);
+        }
+
         $status = Password::sendResetLink(
             $request->only('email')
         );
 
-        // Check if the email was sent successfully and return an appropriate response
         if ($status === Password::RESET_LINK_SENT) {
-            return response()->json(['res'=>'success','msg' => __($status)], 200);
+            return response()->json([
+                'res' => 'success',
+                'msg' => __($status) // Retrieve message from the status
+            ], 200);
         }
 
-        // If the email was not sent successfully, return an error response
-        throw ValidationException::withMessages([
-            'email' => [trans($status)],
-        ]);
+        return response()->json([
+            'res' => 'error',
+            'msg' => __($status), // Retrieve the error message from status
+            'data' => []
+        ], 400);
     }
 }
-
